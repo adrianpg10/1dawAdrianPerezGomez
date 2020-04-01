56a7d77cfce599a516c642888fc1a80d0cb3e775
Adrianpg <adrianperezgomezsmr@gmail.com>
2020-04-01 14:04:45 +0200

    Añade atributos,constructor,getters y setters y toString a la clase Bombo

--- src/main/java/bingo/Bombo.java
+++ src/main/java/bingo/Bombo.java
@@ -5,10 +5,44 @@
  */
 package bingo;
 
+import java.util.Random;
+
 /**
  *
  * @author adrip
  */
 public class Bombo {
-    
+    //Añadimos los atributos correspondientes
+    //Creamos un array de bolas
+
+    private int[] bolas;
+    //Añadimos el atributo numeroBolas y lo pondremos final para que no pueda ser modificado, ya que siempre va a ser el mismo
+    // Si quisiesemos cambiar el numero de bolas, tendriamos que modificar el atributo
+    private final int numeroBolas = 90;
+
+    //Creamos un constructor donde rellenaremos el array con el tamaño del atributo numero de bolas 
+    public Bombo() {
+        this.bolas = new int[numeroBolas];
+
+    }
+    //Getters y Setters
+
+    public int[] getBolas() {
+        return bolas;
+    }
+
+    public void setBolas(int[] bolas) {
+        this.bolas = bolas;
+    }
+
+    public int getNumeroBolas() {
+        return numeroBolas;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        return "Bombo{" + "bolas=" + bolas + ", numeroBolas=" + numeroBolas + '}';
+    }
+
 }
