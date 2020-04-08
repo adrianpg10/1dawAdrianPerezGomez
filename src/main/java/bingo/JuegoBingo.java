7e5de812cfb766e077e81f12979e6beb759e5c20
Adrianpg <adrianperezgomezsmr@gmail.com>
2020-04-08 19:05:15 +0200

    Añadimos método main con los metodos correspondientes en la clase JuegoBingo

--- src/main/java/bingo/JuegoBingo.java
+++ src/main/java/bingo/JuegoBingo.java
@@ -5,10 +5,59 @@
  */
 package bingo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author adrip
  */
 public class JuegoBingo {
-    
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        //Creamos el bombo
+        Bombo bombo = new Bombo();
+        //Creamos el carton
+        Carton carton = new Carton();
+
+        String respuesta = "";
+        //Bucle mientras que la respuesta a la pregunta sea "s" podrá seguir jugando
+        // El bucle para cuando escribe una letra diferente a "s" o canta bingo
+        do {
+            //Mostramos el carton
+            carton.mostrarCarton();
+            //Creamos una variable bola donde almacenará el numero de la bola sacada del bombo
+            int bola = bombo.sacarBola();
+
+            System.out.println("La bola sacada del bombo es: " + bola);
+            //Llamaremos al metodo tacharCasilla y le pasaremos una bola, variable creada anteriormente
+            carton.tacharCasilla(bola);
+            //Llamaremos al comprobarSiLinea donde nos devolverá true o false
+            System.out.println("¿Ha hecho linea? " + carton.comprobarSiLinea());
+            // si comprobarSiLinea es true, cantará linea
+            if (carton.comprobarSiLinea()) {
+                System.out.println("¡¡¡Enhorabuena, has cantado LINEA!!!");
+            }
+            //Llamaremos al comprobarBingo donde nos devolverá true o false
+            System.out.println("¿Ha cantado Bingo? " + carton.comprobarBingo());
+            // si comprobarBingo es true, cantará Bingo
+            if (carton.comprobarBingo()) {
+                System.out.println("¡¡¡Enhorabuena, has cantado BINGO!!!");
+                // Paramos el bucle si cantamos bingo
+                break;
+
+            }
+
+            System.out.println("¿Quieres sacar otra bola del bombo? s/n");
+            respuesta = teclado.nextLine();
+
+        } while (respuesta.equalsIgnoreCase("s"));
+
+        if (!(respuesta.equalsIgnoreCase("s"))) {
+            System.out.println("Vaya, parece que te has rendido, vuelve a jugar más tarde.");
+        }
+
+    }
+
 }
