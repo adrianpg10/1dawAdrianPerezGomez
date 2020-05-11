ee9e9f1ad5249b104c1ac38041d7d85e9994d750
Adrianpg <adrianperezgomezsmr@gmail.com>
2020-05-11 18:23:50 +0200

    Añade atributos y metodos en la clase Carton

--- src/main/java/bingoamericano/Carton.java
+++ src/main/java/bingoamericano/Carton.java
@@ -5,10 +5,75 @@
  */
 package bingoamericano;
 
+import java.awt.Point;
+
 /**
  *
  * @author adrip
  */
-public class Carton {
+public abstract class Carton {
+
+    //Atributos
+    private int[][] carton;
+    private int filas;
+    private int columnas;
+
+    //Constructor parametrizado
+    public Carton(int[][] carton, int filas, int columnas) {
+        this.carton = carton;
+        this.filas = filas;
+        this.columnas = columnas;
+    }
+//Getters y Setters
+
+    public int[][] getCarton() {
+        return carton;
+    }
+
+    public void setCarton(int[][] carton) {
+        this.carton = carton;
+    }
+
+    public int getFilas() {
+        return filas;
+    }
+
+    public void setFilas(int filas) {
+        this.filas = filas;
+    }
+
+    public int getColumnas() {
+        return columnas;
+    }
+
+    public void setColumnas(int columnas) {
+        this.columnas = columnas;
+    }
+
+    //Metodos
+    public abstract void generarCarton();
+
+    public Point tacharNumero(int numero) {
+        return null;
+
+    }
     
+    public boolean esLinea(int numeroFila){
+        return false;
+        
+    }
+    
+    public boolean esBingo(){
+        return false;
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Carton{" + "carton=" + carton + ", filas=" + filas + ", columnas=" + columnas + '}';
+    }
+    
+    
+    
+
 }
