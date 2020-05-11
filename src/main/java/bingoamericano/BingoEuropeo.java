d4988c54e3f6297a864abbcfe30139a61790ea19
Adrianpg <adrianperezgomezsmr@gmail.com>
2020-05-11 16:27:05 +0200

    Añade atributos,getters y setters y toString en la clase BingoEuropeo

--- src/main/java/bingoamericano/BingoEuropeo.java
+++ src/main/java/bingoamericano/BingoEuropeo.java
@@ -5,10 +5,46 @@
  */
 package bingoamericano;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author adrip
  */
-public class BingoEuropeo {
-    
+public final class BingoEuropeo extends Bingo {
+
+    //Atributos
+    private CartonEuropeo carton;
+    private BomboEuropeo bombo;
+
+    //Constructor parametrizado
+    public BingoEuropeo(CartonEuropeo carton, BomboEuropeo bombo, String id, LocalDate fecha, String idJugador) {
+        super(id, fecha, idJugador);
+        this.carton = carton;
+        this.bombo = bombo;
+    }
+
+    //Getters y Setters
+    public CartonEuropeo getCarton() {
+        return carton;
+    }
+
+    public void setCarton(CartonEuropeo carton) {
+        this.carton = carton;
+    }
+
+    public BomboEuropeo getBombo() {
+        return bombo;
+    }
+
+    public void setBombo(BomboEuropeo bombo) {
+        this.bombo = bombo;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        return "BingoEuropeo{" + super.toString() + "carton=" + carton + ", bombo=" + bombo + '}';
+    }
+
 }
