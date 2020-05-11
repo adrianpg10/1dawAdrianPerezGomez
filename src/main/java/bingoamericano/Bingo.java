6ef47cdf5068611e5d6850b47a7bf502b3298077
Adrianpg <adrianperezgomezsmr@gmail.com>
2020-05-11 16:22:47 +0200

    Convierte clase Abstracta, añade atributos, getters y setters y toString en la clase Bingo

--- src/main/java/bingoamericano/Bingo.java
+++ src/main/java/bingoamericano/Bingo.java
@@ -5,10 +5,59 @@
  */
 package bingoamericano;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author adrip
  */
-public class Bingo {
+public abstract class Bingo {
+    //Atributos
+    
+    String id;
+    LocalDate fecha;
+    String idJugador;
+
+    //Constructor parametrizado
+    public Bingo(String id, LocalDate fecha, String idJugador) {
+        this.id = id;
+        this.fecha = fecha;
+        this.idJugador = idJugador;
+    }
+    
+   //Getters y Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getIdJugador() {
+        return idJugador;
+    }
+
+    public void setIdJugador(String idJugador) {
+        this.idJugador = idJugador;
+    }
+
+    //ToString
+    
+    @Override
+    public String toString() {
+        return "Bingo{" + "id=" + id + ", fecha=" + fecha + ", idJugador=" + idJugador + '}';
+    }
+    
+    
     
 }
