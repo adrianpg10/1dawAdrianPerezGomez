aa1968373ec59553fe6c2821fcbf11c26a860375
Adrianpg <adrianperezgomezsmr@gmail.com>
2020-05-11 18:24:33 +0200

    Añade atributos y metodos en la clase CartonAmericano

--- src/main/java/bingoamericano/CartonAmericano.java
+++ src/main/java/bingoamericano/CartonAmericano.java
@@ -9,6 +9,36 @@ package bingoamericano;
  *
  * @author adrip
  */
-public class CartonAmericano {
+public final class CartonAmericano extends Carton {
+    //Atributos
+
+    private static final int FILAS = 5;
+    private static final int COLUMNAS = 5;
+    private Patron patron;
+
+    public CartonAmericano(Patron patron, int[][] carton, int filas, int columnas) {
+        super(carton, filas, columnas);
+        this.patron = patron;
+    }
+
+    //Getters y Setters
+    public Patron getPatron() {
+        return patron;
+    }
+
+    public void setPatron(Patron patron) {
+        this.patron = patron;
+    }
+
     
+    @Override
+    public String toString() {
+        return "CartonAmericano{" + super.toString() + "patron=" + patron + '}';
+    }
+
+    @Override
+    public void generarCarton() {
+        
+    }
+
 }
