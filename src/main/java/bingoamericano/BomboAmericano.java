1bc3aa7bb236ab05de245fb8018dfecad30f9157
Adrianpg <adrianperezgomezsmr@gmail.com>
2020-05-18 13:46:39 +0200

    Modifica constructor, y añade funcionlaidad a llenarBombo en la clase BomboAmericano

--- src/main/java/bingoamericano/BomboAmericano.java
+++ src/main/java/bingoamericano/BomboAmericano.java
@@ -5,6 +5,8 @@
  */
 package bingoamericano;
 
+import java.util.Collections;
+
 /**
  *
  * @author adrip
@@ -16,8 +18,7 @@ public final class BomboAmericano extends Bombo {
 
     //Constructor
     public BomboAmericano() {
-        //Llamada al metodo llenarBombo y le pasamos la cantidadBolas por parametro
-        llenarBombo();
+        super();
     }
 
     //Getter
@@ -25,13 +26,15 @@ public final class BomboAmericano extends Bombo {
         return cantidadBolas;
     }
 
-    //Metodo sobreescrito para llenar de bolas el bomboAmericano
+    //Metodo sobreescrito para llenar de bolas el bomboAmericano, y las mezcla
     @Override
     public void llenarBombo() {
 
         for (int i = 0; i < cantidadBolas; i++) {
             getBolas().add(i + 1);
         }
+
+        Collections.shuffle(super.getBolas());
     }
 
 }
