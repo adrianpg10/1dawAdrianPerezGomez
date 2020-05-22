74ca4bbfc9869c3d7d47aa6cf53f7f4b1e0ff403
Adrianpg <adrianperezgomezsmr@gmail.com>
2020-05-22 18:16:26 +0200

    Primera Modificacion del JuegoBingo

--- src/main/java/bingoamericano/JuegoBingo.java
+++ src/main/java/bingoamericano/JuegoBingo.java
@@ -5,10 +5,24 @@
  */
 package bingoamericano;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author adrip
  */
 public class JuegoBingo {
-    
+
+    public static void main(String[] args) {
+
+        CartonAmericano cartonAmericano = new CartonAmericano();
+        BomboAmericano bomboAmericano = new BomboAmericano();
+
+        cartonAmericano.generarCarton();
+        BingoAmericano BingoAmericano = new BingoAmericano(cartonAmericano, bomboAmericano, "1", LocalDate.now(), "J01");
+
+        BingoAmericano.getCarton().mostrarCarton();
+
+    }
+
 }
